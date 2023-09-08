41cfa0f7568d734e8137a04c370d8ba50d89920f
Z Luo <57984887+lzqv5@users.noreply.github.com>
2023-09-08 16:33:51 +0800

    Update run_stage_2.py with a comment_prompts func.

--- run_stage_2.py
+++ run_stage_2.py
@@ -16,13 +16,14 @@ from langchain.prompts.chat import (
 
 
 os.environ["OPENAI_API_KEY"] = "API_KEY" 
+summarizer = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.3, n=1, max_tokens=512)
 
 # output parser
 class JsonExtractor():
 
     def __init__(self) -> None:
         self.pattern_json_extractor = re.compile(r'{[^\{\}]+}')
-        self.pattern_remove_space = re.compile(r'\n\s{0,}') 
+        self.pattern_remove_space = re.compile(r'\n\s{0,}')
         self.pattern_refine = re.compile(r'(?<![a-zA-Z])\'(?=[a-zA-Z])|(?<=[a-zA-Z])\'(?![a-zA-Z])')
         self.keys = ["overall", "education", "education comment", "experience", "experience comment", "skill", "skill comment"]
         self.descriptions = ["Total score", "Educational score", "comment on the educational score", "work experience score", "comment on the work experience score", "skill score", "comment on the skill score"]
@@ -42,7 +43,8 @@ class JsonExtractor():
         return '```json'+text+'```'
     
     def __call__(self, text: str) -> dict:
-        # parse LLM output
+
+        # 用于解析 LLM output
         text = self._remove_space(self._extract(text))
         try:
             return literal_eval(text)
@@ -50,7 +52,6 @@ class JsonExtractor():
             print('wrong text format:\n', text)
             return literal_eval(self._refine_property_name_quotes(text))
 
-# 按需构造 prompt template
 def build_chat_prompt(prompt_idx=0):
     human_message_prompt = """你是一个专业的HR，给定一对(职位描述,简历)，你能够判断该岗位描述和该简历是否匹配并进行评论。
 
@@ -88,11 +89,11 @@ def build_chat_prompt(prompt_idx=0):
         你的第一个回复应该只是“已理解”。
         """
     elif prompt_idx == 1:
-        human_message_prompt += """to be implemented(#1)
+        human_message_prompt += """请根据提供的简历和岗位描述，按照上述格式输出多个0-100之间的各字段匹配分数。你可以处理各种类型的文本，并且请在没有足够信息的情况下给出合理的反馈。
         你的第一个回复应该只是“已理解”。
         """
     else:
-        human_message_prompt += """to be implemented(#2)
+        human_message_prompt += """你需要按照“教育背景”、“工作经历”和“专业技能”三个指标，基于简历和岗位的匹配程度，返回三个指标的匹配分数，并返回一个能总体上反映简历与岗位匹配程度的综合分数。
         你的第一个回复应该只是“已理解”。
         """
     assistant_message_prompt = "已理解。"
@@ -110,7 +111,27 @@ def build_chat_prompt(prompt_idx=0):
     chat_prompt= ChatPromptTemplate.from_messages([human_message_prompt_template, assistant_message_prompt_template, input_prompt_template])
     return chat_prompt
 
-def rebuild_output(mean_score, outputs):
+def build_comment_prompt():
+    summarize_prompt = """你是一个专业的HR。现在，有数位专家对同一份简历进行了评论。你需要根据这些评论，生成一段简短的评语，来描述这份简历的优势和不足。
+
+    输出结果以如下键值对的JSON格式表示:
+    ```
+    "comments": a comment paragraph
+    ```
+
+    以下是这些专家的评论:
+    ```
+    {comments}
+    ```
+
+    请输出JSON格式的结果
+    """
+    prompt= ChatPromptTemplate.from_messages([HumanMessagePromptTemplate.from_template(summarize_prompt)])
+    return prompt
+
+cmts_prompt = build_comment_prompt()
+
+def rebuild_output(mean_score, outputs, output_parser):
     flag = 1e3
     idx = 0
     for i, output in enumerate(outputs):
@@ -120,18 +141,39 @@ def rebuild_output(mean_score, outputs):
             idx = i
     picked_output = outputs[idx]
     picked_output['overall'] = mean_score
+
+    edu_cmts = ''
+    exp_cmts = ''
+    ski_cmts = ''
+    for i in range(len(outputs)):
+        edu_cmts += 'education comment' + outputs[i]['education comment'] + '\n'
+        exp_cmts += 'experience comment' + outputs[i]['experience comment'] + '\n'
+        ski_cmts += 'skill comment' + outputs[i]['skill comment'] + '\n'
+    edu_cmts = summarizer(cmts_prompt.format_prompt(comments=edu_cmts).to_messages())
+    picked_output['education comment'] = output_parser(edu_cmts.content)
+    exp_cmts = summarizer(cmts_prompt.format_prompt(comments=exp_cmts).to_messages())
+    picked_output['experience comment'] = output_parser(exp_cmts.content)
+    ski_cmts = summarizer(cmts_prompt.format_prompt(comments=ski_cmts).to_messages())
+    picked_output['skill comment'] = output_parser(ski_cmts.content)
     return picked_output
 
 def single_session_processing(generation: list, output_parser: JsonExtractor):
     n = len(generation)
     outputs = [output_parser(generation[i].text) for i in range(n)]
     mean_score = sum([d.get('overall', 0) for d in outputs])/n
-    return rebuild_output(mean_score, outputs)
+    return rebuild_output(mean_score, outputs, output_parser)
 
 def sessions_processing(generations: list, output_parser: JsonExtractor):
     return [single_session_processing(gen, output_parser) for gen in generations]
 
+#! 付费用户使用频率 - 3,500 RPM or 90,000 TPM - chat
+#! 假设每次调用token数有预估, 那么 TPM 可以转化为等价的 RPM
+
+# 给定 jd, cv 和 candidates, 自动化地生成大模型排序后的结果
 def resorting(jd, cv, candidates, chat_model, chat_prompt, output_parser,  rate_limit_per_min=3500, tokens_per_min=90000, token_per_req=4000, delta=2.5):
+    #! 每执行一次 resorting，就调用一次或者多次 LLM api;
+    #! 调用 api 的实际频率要看实际调用 OpenAI API 的频率，而不是 LangChain 调用的频率;
+    #! 需要限制 resorting 的调用频率
     if not isinstance(chat_prompt, list):
         # 只有一个 prompt
         n = chat_model.n
@@ -146,12 +188,12 @@ def resorting(jd, cv, candidates, chat_model, chat_prompt, output_parser,  rate_
             end_time = time()
             diff = end_time-start_time
             if diff < 60:
+                print("sleeping...")
                 sleep(60-diff)
-
 
         outputs = sessions_processing(generations, output_parser)
         cands_new = [item for item in zip(candidates,outputs)]
-    else:   # ensemble
+    else:   
         prompt_nums = len(chat_prompt)
         candidates_nums = len(candidates)
         n = chat_model.n
@@ -162,18 +204,20 @@ def resorting(jd, cv, candidates, chat_model, chat_prompt, output_parser,  rate_
             messages_list = [[prompt.format_prompt(jd=jd, cv=cand).to_messages() for cand in candidates[i:i+step]] for i in range(0,candidates_nums,step)]
             generations = []
             for messages in messages_list:  # 1 batch/min
+                # print("Begin")
                 start_time = time()
                 responses_of_generation = chat_model.generate(messages)
                 generations.extend(responses_of_generation.generations)
                 end_time = time()
+                # print("End")
                 diff = end_time-start_time
                 if diff < 60:
+                    print("sleeping...")
                     sleep(60-diff)
             outputs.append(sessions_processing(generations, output_parser))
 
         mean_scores = [sum([outputs[i][j].get('overall',0) for i in range(prompt_nums)])/prompt_nums for j in range(candidates_nums)]
-
-        outputs_new = [rebuild_output(mean_score=mean_scores[j], outputs=[outputs[i][j] for i in range(prompt_nums)]) for j in range(candidates_nums)]
+        outputs_new = [rebuild_output(mean_score=mean_scores[j], outputs=[outputs[i][j] for i in range(prompt_nums)], output_parser=output_parser) for j in range(candidates_nums)]
         cands_new = [item for item in zip(candidates, outputs_new)]
 
     cands_new.sort(key=lambda item:item[1].get('overall',0),reverse=True)
@@ -183,7 +227,8 @@ def resorting(jd, cv, candidates, chat_model, chat_prompt, output_parser,  rate_
 def resort_self_consistency(chat_model, recs, output_parser: JsonExtractor):
     chat_prompt = build_chat_prompt(prompt_idx=0)
     resorted_recs = []
-    for jd, cv, cands in tqdm(recs):
+    for idx, (jd, cv, cands, _) in enumerate(recs):
+        print(f"idx: {args.start_idx+idx} - processing...")
         resorted_recs.append(resorting(jd, cv, cands, chat_model, chat_prompt, output_parser))
         with open(f'data/recs_self_consistency_(cv{args.cv_len}_jd500)_temperature_{args.temperature}_n_{args.n}.json', 'w') as f:
             json.dump(resorted_recs, f, ensure_ascii=False)
@@ -192,7 +237,9 @@ def resort_self_consistency(chat_model, recs, output_parser: JsonExtractor):
 def resort_ensemble(chat_model, recs, output_parser: JsonExtractor, prompt_ids=[0,1,2], rate_limit_per_min=3, delta=2.5):
     chat_prompts = [build_chat_prompt(prompt_idx=idx) for idx in prompt_ids]
     resorted_recs = []
-    for jd, cv, cands in tqdm(recs):
+    for idx, (jd, cv, cands, _) in enumerate(recs):
+        # start_time = time()
+        print(f"idx: {args.start_idx+idx} - processing...")
         resorted_recs.append(resorting(jd, cv, cands, chat_model, chat_prompts, output_parser))
         with open(f'data/recs_ensemble_(cv{args.cv_len}_jd500)_temperature_{args.temperature}_n_{args.n}.json', 'w') as f:
             json.dump(resorted_recs, f, ensure_ascii=False)
